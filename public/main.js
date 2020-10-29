a5896179d60fec4342153a34ef8949c13b08f296
Taki <laszlo@letmewebyou.com>
2020-10-29 15:26:51 +0100

    Added change view button

--- public/main.js
+++ public/main.js
@@ -7,19 +7,24 @@ var image = document.querySelector('#new-employee').children[2]
 var email = document.querySelector('#new-employee').children[2]
 var lastPass = document.querySelector('#new-employee').children[2]
 var title = document.querySelector('#new-employee').children[2]
+
+
 var addNewEmployee = document.querySelector('#add-new-employee');
 const x = document.querySelector('#new-employee');
 
 addNewEmployee.addEventListener('click', _ => {
   if (x.style.display === "block") {
     x.setAttribute("style", "animation-name: bwd;");
+    addNewEmployee.innerHTML = "Add new employee"
     addSleep()
 
 
   } else {
     x.setAttribute("style", "animation-name: fwd;");
     x.style.display = "block";
-    console.log("ANYAD1")
+    addNewEmployee.innerHTML = "Cancel"
+
+
 
   }
 })
@@ -53,15 +58,21 @@ deleteEmployee.addEventListener('click', _ => {
   } else {
     y.setAttribute("style", "animation-name: fwd1;");
     y.style.display = "block"
+    y.style.visibility = "visible"
     deleteEmployee.innerHTML = "Cancel"
+
   }
 })
 
 async function delSleep() {
   await sleep1(500);
 
-  y.style.display = "none";
+  y.style.visibility = 'hidden';
 }
+
+
+
+// ASK PROMPT ON DELETE
 const delButton = document.querySelector('#delete-entry-button')
 delButton.addEventListener('click', _ => {
 
@@ -78,6 +89,30 @@ delButton.addEventListener('click', _ => {
     })
   } else if (answer === 'no' || 'No' || 'nem' || 'Nem' || 'NO') {
     location.reload()
+  }
+
+
+})
+
+
+
+
+// CHANGE TO TABLE VIEW OR CARD VIEW (VICA VERSA)
+
+const changeView = document.querySelector('#change-view')
+const employeeTable = document.querySelector('#employee-table')
+const employeeCard = document.querySelector('#hide-cards')
+changeView.addEventListener('click', _ => {
+
+  if (employeeCard.style.display != "none") {
+    employeeCard.style.display = "none";
+    employeeTable.style.display = "block"
+    changeView.innerHTML = "Card View"
+
+  } else if (changeView.innerHTML === "Card View") {
+    employeeCard.style.display = "block";
+    employeeTable.style.display = "none"
+    changeView.innerHTML = "Table View"
   }
 
 
